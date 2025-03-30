c76af7c08e5b837e4f2cfcbd2a9a0a2212d2dea7
yoonju1030 <allrealjgt@gmail.com>
2025-03-30 21:45:43 +0900

    set dotenv

--- src/global_knowledge/db.py
+++ src/global_knowledge/db.py
@@ -3,7 +3,10 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 
-DATABASE_URL = f"postgresql://{os.environ['DB_ID']}:{os.environ['DB_PW']}@{os.environ['DB_HOST']}/{os.environ['DB_NAME']}"
+from dotenv import load_dotenv
+load_dotenv()
+
+DATABASE_URL = f'postgresql://{os.environ["DB_ID"]}:{os.environ["DB_PW"]}@{os.environ["DB_HOST"]}/{os.environ["DB_NAME"]}'
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
